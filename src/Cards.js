825abf5c7a8a907dfc1dd1a577b2348d1d474e78
Kayla Lawson <kaylarae7128@gmail.com>
2019-04-04 18:08:39 -0600

    Worked on basic style for card css

--- src/Cards.js
+++ src/Cards.js
@@ -1,17 +1,18 @@
 import React from 'react';
-
+import './Cards.css';
 const Cards = () => {
   return (
-    <section> 
-      <img src="prop-stuff" alt="prop-stuff" />
+    <section className="cards"> 
+      <img src="https://c402277.ssl.cf1.rackcdn.com/photos/1341/images/hero_full/snow-leopard-test.jpg?1345592123" alt="prop-stuff" />
       <article>
         {/* favorite an animal */}
         <h2>Animal Name</h2>
-        <p>Extinction Level</p>
-        <p>Population</p>
-        <p>Genus</p>
-        <p>Locations</p>
-        <p>Threats</p>
+        <p>Extinction Level: </p>
+        <p>Population: </p>
+        <p>Genus: </p>
+        <p>Locations: </p>
+        <p>Threats: </p>
+        <p>Get Involved: </p>
       </article>
     </section>
   )
