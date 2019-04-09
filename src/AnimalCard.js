a53ffbadd454958c0379dc575aaba0f45ad1ba79
DeMarcus Kirby <tkd024@gmail.com>
2019-04-09 10:14:51 -0600

    added text swap functionality to favorite button

--- src/AnimalCard.js
+++ src/AnimalCard.js
@@ -6,14 +6,21 @@ const AnimalCard = (props) => {
     <section className="cards"> 
       <img src={props.image} />
       <article>
-        <button onClick={() => props.addFav(props.name)}>Add to Favorites</button>
+        <button onClick={(e) => {
+          props.addFav(props.name)
+          if(e.target.innerText === 'Add to Favorites'){
+            e.target.innerText = 'Favorited!';
+          } else{
+            e.target.innerText = 'Add to Favorites'
+          }
+        }
+         } id='favbtn'>Add to Favorites</button>
         <h2>{props.name}</h2>
         <p className="animal-info-titles">Extinction Level: <span className="animal-info-red">{props.endangeredStatus}</span></p>
         <p className="animal-info-titles">Population: <span className="animal-info-red">{props.population}</span></p>
         <p className="animal-info-titles">Genus: <span className="animal-info">{props.genus}</span></p>
         <p className="animal-info-titles">Locations: <span className="animal-info">{props.locations.join(', ')}</span></p>
         <p className="animal-info-titles">Threats: <span className="animal-info">{props.threats.join(', ')}</span></p>
-        <p className="animal-info-titles">Get Involved:</p>
       </article>
     </section>
   )
