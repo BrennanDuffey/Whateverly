7eb4d73d60b42df244add87286b4c676d6c08d5c
Kayla Lawson <kaylarae7128@gmail.com>
2019-04-10 17:02:44 -0600

    Changed countryCard threats name

--- src/CountryCard.js
+++ src/CountryCard.js
@@ -7,7 +7,7 @@ const CountryCard = (props) => {
     <article className="cc-card">
       <h2>{props.name}</h2>
       <p className="cc">Homo-Sapian Population: <span className="cc-pop">{props.population}</span></p>
-      <p className="cc">Environmental Threats: <span className="cc-info">{props.topThreats.join(', ')}</span></p>
+      <p className="cc">Threats to Wildlife: <span className="cc-info">{props.topThreats.join(', ')}</span></p>
       <p className="cc">Conservation Groups: <span className="cc-info">{props.conservationGroups.map(group => {
         return <Link link={group.link} name={group.name} />
       })}</span></p>
