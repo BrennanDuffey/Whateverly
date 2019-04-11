d388dee2e1606aac454813a14fc68cf765a8f50a
Brennan <brennanduffey@gmail.com>
2019-04-11 07:45:11 -0600

    Added tests for App methods

--- src/App.test.js
+++ src/App.test.js
@@ -20,6 +20,39 @@ describe('App', () => {
       expect(wrapper).toMatchSnapshot();
   });
 
-  
+  it('Should have default states', () => {
+    expect(wrapper.state()).toEqual({
+      endangeredSpecies: [],
+      countries: [],
+      searchInput: '',
+      favAnimals: [],
+      isFavorite: false
+    });
+  });
+
+  it('Should be able to update favAnimals state', () => {
+    expect(wrapper.state().favAnimals).toEqual([]);
+    wrapper.instance().addFav('Jaguar');
+    expect(wrapper.state().favAnimals).toEqual(['Jaguar']);
+  });
+
+  it('Should be able to update favAnimals state', () => {
+    expect(wrapper.state().favAnimals).toEqual(['Jaguar']);
+    wrapper.instance().addFav('Jaguar');
+    expect(wrapper.state().favAnimals).toEqual([]);
+  });
+
+  it('Should be able to update searchInput state', () => {
+    expect(wrapper.state().searchInput).toEqual('');
+    wrapper.instance().generateContent('Jaguar');
+    expect(wrapper.state().searchInput).toEqual('Jaguar');
+  });
+
+
+  it('Should be able to update isFavorite state', () => {
+    expect(wrapper.state().isFavorite).toEqual(false);
+    wrapper.instance().setFavoriteState(true);
+    expect(wrapper.state().isFavorite).toEqual(true);
+  });
 });
 
